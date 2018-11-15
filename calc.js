37fe75696d3b135ae92c995246b7c6168fd7565b
Andrew <vandyand@gmail.com>
2018-11-15 13:51:13 -0500

    added unicode symbols

--- calc.js
+++ calc.js
@@ -1,25 +1,18 @@
 $(function(){
 //-------------------------------------------------------------------------
   //Set up the keys and display area
-  $('div').addClass('row').append('<div></div><div></div><div></div>')
-  $('div div:eq(0)').addClass('col-2');
-  $('div div:eq(1)').addClass('col-8');
-  $('div div:eq(2)').addClass('col-2');
-  $('div div:eq(1)').append('<div></div><div></div><div></div><div></div><div></div>');
-
   $('div div div').each(function(){
     $(this).addClass('row no-gutters');
-    $(this).append('<div></div><div></div><div></div><div></div>');
   })
 
-  // This is for testing
-  // $('div:last').after('<span id="first"></span><br><span id="second"></span>');
-
   var counter = 0;
-  var map = ['C','','^','*','7','8','9','/','4','5','6','-','1','2','3','+','0','.','','='];
+  var map = ['C','%','^','&#215;','7','8','9','&#247;','4','5','6','-','1','2','3','+','&#177;','0','.','='];
+  //&#215; is times, &#247; is divide, 	&#177; is plus_minus
+  var map = ['C','%','^','&#215;','7','8','9','&#247;','4','5','6','-','1','2','3','+','&#177;','0','.','='];
   $('div div div div').each(function(){
     $(this).addClass('btn btn-outline-secondary col-3');
-    $(this)[0].textContent = map[counter];
+    $(this).attr('id',map[counter]);
+    $(this)[0].innerHTML = map[counter];
     counter += 1;
   });
   //display area!
@@ -37,6 +30,7 @@ $(function(){
   var new_op = false;
 
   $('div div div div').on('click', function(e){
+    //If button pressed is a number or decimal...
     if($.inArray(e.target.textContent,['.','0','1','2','3','4','5','6','7','8','9']) != -1){
       if(replace_prev_value){
         $('p')[0].textContent = '';
@@ -46,40 +40,53 @@ $(function(){
       $('p').append(e.target.textContent);
       count = 0;
     }
-    if($.inArray(e.target.textContent,['+','-','*','/','^']) != -1){
-      operation = e.target.textContent;
+    //If button pressed is an operator...
+    if($.inArray(e.target.id,['+','-','&#215;','&#247;','^']) != -1){
+      operation = e.target.id;
       replace_prev_value = true;
       first_val = $('p')[0].textContent;
       // $('#first')[0].textContent = first_val;
     }
+    //If equals button pressed...
     if(e.target.textContent==='='){
       replace_prev_value = true;
       new_op = true;
       current_val = $('p')[0].textContent;
       if(count===0){
         second_val = current_val;
-        // $('#second')[0].textContent = second_val;
       }
       else{first_val = current_val;}
-      // $('#first')[0].textContent = first_val;
       count += 1;
+      //Assign operations based on operation selected...
       if(operation==='+'){ret_val = parseFloat(first_val) + parseFloat(second_val);}
       else if(operation==='-'){ret_val = parseFloat(first_val) - parseFloat(second_val);}
-      else if(operation==='/'){ret_val = parseFloat(first_val) / parseFloat(second_val);}
-      else if(operation==='*'){ret_val = parseFloat(first_val) * parseFloat(second_val);}
+      else if(operation==='&#247;'){ret_val = parseFloat(first_val) / parseFloat(second_val);}
+      else if(operation==='&#215;'){ret_val = parseFloat(first_val) * parseFloat(second_val);}
       else if(operation==='^'){ret_val = parseFloat(first_val) ** parseFloat(second_val);}
       else{ret_val = current_val;}
       $('p')[0].textContent = ret_val;
     }
+    //If Clear button pressed...
     if(e.target.textContent==='C'){
       first_val = '';
       second_val = '';
       current_val = '';
       ret_val = 0;
-      // $('#first')[0].textContent = first_val;
-      // $('#second')[0].textContent = second_val;
       $('p')[0].textContent = '';
+    }
+    //If ± button pressed...
+    if(e.target.id==='&#177;'){
+      $('p')[0].textContent = parseFloat($('p')[0].textContent) * -1;
+    }
+    //If % button pressed...
+    if(e.target.id==='%'){
+      $('p')[0].textContent = parseFloat($('p')[0].textContent) / 100;
     }
   })
 //--------------------------------------------------------------------------
+  $('#id1').on('click',function(e){
+    $('#id2')[0].textContent = e.target.textContent;
+  })
+
+
 })
