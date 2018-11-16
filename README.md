0dbb24552c9f3b30998489c6dd7e83dfef8becc2
Andrew <vandyand@gmail.com>
2018-11-16 11:41:26 -0500

    added styles and verified functionality across browsers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Simple calculator using jquery and bootstrap. Practicing concepts learned from this excellent [javascript and jquery book.](http://javascriptbook.com/buy/)
+See project [here.](https://jsfiddle.net/axdbcq6z/3/)
+Tested on Chrome, Firefox, Edge and Opera
--- calc.js
+++ calc.js
@@ -1,40 +1,42 @@
 $(function(){
-//-------------------------------------------------------------------------
-  //Set up the keys and display area
+  //---------------------------------------------------------------------------
+  // Setup display area and buttons
+  //---------------------------------------------------------------------------
 
+  // Display bar setup
+  $('button:eq(0)').attr('id','display');
+  $('button:eq(0)').attr('class','col-12 display-bar no-padding btn btn-outline-secondary text-right');
+  $('button:eq(0)')[0].innerHTML = '&ensp;';
+
+  // Symbol map: &#215; is ×, &#247; is ÷, &#177; is ±
+  var map = ['C','%','^','&#247;','7','8','9','&#215;','4','5','6','-','1','2','3','+','&#177;','0','.','='];
   var counter = 0;
-  //&#215; is × symbol, &#247; is ÷ symbol, 	&#177; is ± symbol
-  var map = ['','C','%','^','&#247;','7','8','9','&#215;','4','5','6','-','1','2','3','+','&#177;','0','.','='];
 
-  $('button').each(function(){
-    //$(this).attr('class','btn btn-outline-secondary btn-block');
+  // Input buttons setup
+  $('button:gt(0)').each(function(){
     $(this).attr('id',map[counter]);
-    $(this).attr('style','height:15%;width:100%;');
-    $(this).attr('class','btn btn-outline-secondary');
+    //$(this).attr('style','height:12.5%;width:25%;padding:0 !important;margin:0 !important;');
+    $(this).attr('class','col-3 no-padding input-btn btn btn-outline-secondary');
     $(this)[0].innerHTML = map[counter];
     counter += 1;
   })
 
-  $('button:first').attr('id','display');
-  $('button:first').attr('style','height:15%;width:100%;');
-  $('button:first').addClass('text-right');
-  //$('button:first').attr('class','border')
-  //Display area...
-  //$('p').attr('class','border border-secondary rounded text-right flex-fill').attr('id','display').attr('style','padding:1%');
-//-------------------------------------------------------------------------
-
-  // Business Logic!
+  //---------------------------------------------------------------------------
+  // Business Logic
+  //---------------------------------------------------------------------------
   var first_val = '';
   var second_val = '';
   var current_val = '';
   var ret_val = 0;
   var operation = '';
-  var replace_prev_value = false;
+  var replace_prev_value = true;
   var count = 0;
   var new_op = false;
+  var before_equals = true;
 
   $('button').on('click', function(e){
-    //If button pressed is a number or decimal...
+
+    // Number button logic
     if($.inArray(e.target.id,['.','0','1','2','3','4','5','6','7','8','9']) != -1){
       if(replace_prev_value){
         $('#display')[0].textContent = '';
@@ -44,15 +46,14 @@ $(function(){
       $('#display').append(e.target.textContent);
       count = 0;
     }
-    //If button pressed is an operator...
-    if($.inArray(e.target.id,['+','-','&#215;','&#247;','^']) != -1){
-      operation = e.target.id;
-      replace_prev_value = true;
-      first_val = $('#display')[0].textContent;
-      // $('#first')[0].textContent = first_val;
+
+    // Equals button logic
+    if(e.target.id==='='){
+      before_equals = false;
     }
-    //If equals button pressed...
-    if(e.target.textContent==='='){
+
+    // Operation logic
+    if(e.target.id==='='||($.inArray(e.target.id,['+','-','&#215;','&#247;','^']) != -1 && first_val!=='' && before_equals===true)){
       replace_prev_value = true;
       new_op = true;
       current_val = $('#display')[0].textContent;
@@ -61,7 +62,7 @@ $(function(){
       }
       else{first_val = current_val;}
       count += 1;
-      //Assign operations based on operation selected...
+      // Assign functionality based on selected operation
       if(operation==='+'){ret_val = parseFloat(first_val) + parseFloat(second_val);}
       else if(operation==='-'){ret_val = parseFloat(first_val) - parseFloat(second_val);}
       else if(operation==='&#247;'){ret_val = parseFloat(first_val) / parseFloat(second_val);}
@@ -70,22 +71,31 @@ $(function(){
       else{ret_val = current_val;}
       $('#display')[0].textContent = ret_val;
     }
-    //If Clear button pressed...
-    if(e.target.textContent==='C'){
+
+    // Exclusive operation button logic
+    if($.inArray(e.target.id,['+','-','&#215;','&#247;','^']) != -1){
+      operation = e.target.id;
+      replace_prev_value = true;
+      first_val = $('#display')[0].textContent;
+      before_equals = true;
+    }
+
+    // Clear button logic
+    if(e.target.id==='C'){
       first_val = '';
       second_val = '';
       current_val = '';
+      operation = '';
       ret_val = 0;
-      $('#display')[0].textContent = '';
+      $('#display')[0].innerHTML = '&ensp;';
     }
-    //If ± button pressed...
+    // Plus/Minus (±) button logic
     if(e.target.id==='&#177;'){
       $('#display')[0].textContent = parseFloat($('#display')[0].textContent) * -1;
     }
-    //If % button pressed...
+    // Percent button logic
     if(e.target.id==='%'){
       $('#display')[0].textContent = parseFloat($('#display')[0].textContent) / 100;
     }
   })
-//--------------------------------------------------------------------------
 })
